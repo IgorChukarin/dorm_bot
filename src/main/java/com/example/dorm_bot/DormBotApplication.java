249b2607da095b0b9d8cf4177affe5a0fc3dd22f
IgorChukarin <Zigor2356@mail.ru>
2025-03-08 15:44:05 +0100

    .env file created

--- src/main/java/com/example/dorm_bot/DormBotApplication.java
+++ src/main/java/com/example/dorm_bot/DormBotApplication.java
@@ -1,5 +1,6 @@
 package com.example.dorm_bot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,11 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class DormBotApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("TELEGRAM_BOT_TOKEN", dotenv.get("TELEGRAM_BOT_TOKEN"));
+		System.setProperty("TELEGRAM_BOT_USERNAME", dotenv.get("TELEGRAM_BOT_USERNAME"));
+
 		SpringApplication.run(DormBotApplication.class, args);
 	}
 
