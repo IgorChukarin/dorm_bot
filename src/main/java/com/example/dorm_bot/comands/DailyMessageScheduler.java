a68942962cff1b138ae01c2c0c10159e4bf9ecda
IgorChukarin <Zigor2356@mail.ru>
2025-03-27 22:33:31 +0100

    scheduler works once a day, added phrase generation

--- src/main/java/com/example/dorm_bot/comands/DailyMessageScheduler.java
+++ src/main/java/com/example/dorm_bot/comands/DailyMessageScheduler.java
@@ -1,12 +1,15 @@
 package com.example.dorm_bot.comands;
 
 import com.example.dorm_bot.Bot;
+import com.example.dorm_bot.repositories.ProductRepository;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -15,35 +18,46 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class DailyMessageScheduler {
     private final Bot bot;
+    private final ProductRepository productRepository;
     private final String chatId = "-4624440055";
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final List<String> startPhrases = Arrays.asList(
+            "E che cazzo, ragazzi!",
+            "Porco dio, ragazzi!",
+            "Dio lupo, ragazzi!",
+            "Buongiorno, stronzi!"
+    );
 
-    public DailyMessageScheduler(Bot bot) {
+    public DailyMessageScheduler(Bot bot, ProductRepository productRepository) {
         this.bot = bot;
+        this.productRepository = productRepository;
         scheduleDailyMessage();
     }
 
     private void scheduleDailyMessage() {
-        SendMessage response = new SendMessage();
-        response.setChatId(chatId);
-        response.setText("Hello!");
         Runnable task = () -> {
+            List<String> products = productRepository.getAllProducts();
+            if (products.isEmpty()) {
+                return;
+            }
             try {
+                SendMessage response = new SendMessage();
+                String messageText = generateMessageText();
+                response.setChatId(chatId);
+                response.setText(messageText);
                 bot.execute(response);
             } catch (TelegramApiException e) {
                 throw new RuntimeException(e);
             }
         };
         long initialDelay = computeInitialDelay();
-        scheduler.schedule(task, initialDelay, TimeUnit.HOURS);
+        scheduler.scheduleAtFixedRate(task, initialDelay, 1, TimeUnit.DAYS);
     }
 
 
     private long computeInitialDelay() {
         LocalDateTime now = LocalDateTime.now();
-        int hours = getRandomNumberInRange(8, 16);
-        int minutes = getRandomNumberInRange(0, 59);
-        LocalDateTime nextRun = now.withHour(hours).withMinute(minutes);
+        LocalDateTime nextRun = now.withHour(8).withMinute(0);
         if (now.isAfter(nextRun)) {
             nextRun = nextRun.plusDays(1);
         }
@@ -54,8 +68,24 @@ public class DailyMessageScheduler {
         Random random = new Random();
         return random.nextInt(max - min + 1) + min;
     }
+
+    private String generateMessageText() {
+        String startPhrase = startPhrases.get(getRandomNumberInRange(0,startPhrases.size()));
+        List<String> products = productRepository.getAllProducts();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < products.size(); i++) {
+            sb.append(i + 1).append(". ").append(products.get(i)).append("\n");
+        }
+        String productsColumn = sb.toString().trim();
+        return startPhrase +
+                "\n\n" +
+                "You are completely out of:" +
+                "\n" +
+                productsColumn +
+                "\n\n" +
+                "You have to buy it!";
+    }
 }
 
 // tag someone random
-// some random phrase
 // counter
