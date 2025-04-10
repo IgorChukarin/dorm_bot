9cba27420ecda7c85849e9ef9ff295947cff9130
IgorChukarin <Zigor2356@mail.ru>
2025-04-10 16:01:30 +0200

    debugging

--- src/main/java/com/example/dorm_bot/DormBotApplication.java
+++ src/main/java/com/example/dorm_bot/DormBotApplication.java
@@ -19,18 +19,18 @@ public class DormBotApplication {
 		SpringApplication app = new SpringApplication(DormBotApplication.class);
 
 
-//		Dotenv dotenv = Dotenv.load();
-//		System.setProperty("TELEGRAM_BOT_TOKEN", dotenv.get("TELEGRAM_BOT_TOKEN"));
-//		System.setProperty("TELEGRAM_BOT_USERNAME", dotenv.get("TELEGRAM_BOT_USERNAME"));
-//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("TELEGRAM_BOT_TOKEN", dotenv.get("TELEGRAM_BOT_TOKEN"));
+		System.setProperty("TELEGRAM_BOT_USERNAME", dotenv.get("TELEGRAM_BOT_USERNAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
-		Map<String, Object> properties = new HashMap<>();
-		properties.put("bot.token", System.getenv("TELEGRAM_BOT_TOKEN"));
-		properties.put("bot.username", System.getenv("TELEGRAM_BOT_USERNAME"));
-		properties.put("spring.datasource.username", System.getenv("DB_USERNAME"));
-		properties.put("spring.datasource.password", System.getenv("DB_PASSWORD"));
-		app.setDefaultProperties(properties);
+//		Map<String, Object> properties = new HashMap<>();
+//		properties.put("bot.token", System.getenv("TELEGRAM_BOT_TOKEN"));
+//		properties.put("bot.username", System.getenv("TELEGRAM_BOT_USERNAME"));
+//		properties.put("spring.datasource.username", System.getenv("DB_USERNAME"));
+//		properties.put("spring.datasource.password", System.getenv("DB_PASSWORD"));
+//		app.setDefaultProperties(properties);
 
 
 		app.run(args);
